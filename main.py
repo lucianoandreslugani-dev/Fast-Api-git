723f0e6abafcdc3f20be3e5322ed1ee9f0915d8f
luganisofia <lucianoandreslugani@gmail.com>
2026-04-20 08:58:48 -0300

    crud -3

--- main.py
+++ main.py
@@ -14,3 +14,23 @@ def listar_productos():
     return {"productos": productos}
 
 
+@app.post("/productos")
+def agregar_producto(producto: Producto):
+    productos.append(producto)
+    return {"mensaje": f"Producto agregado correctamente", "producto": producto}
+
+
+@app.put("/productos/{id}")
+def actualizar_producto(id: int, producto: Producto):
+    if id < 0 or id >= len(productos):
+        return {"error": "Producto no encontrado"}
+    productos[id] = producto
+    return {"mensaje": "Producto actualizado correctamente", "producto": producto}
+
+
+@app.delete("/productos/{id}")
+def eliminar_producto(id: int):
+    if id < 0 or id >= len(productos):
+        return {"error": "Producto no encontrado"}
+    producto_eliminado = productos.pop(id)
+    return {"mensaje": "Producto eliminado correctamente", "producto": producto_eliminado}

8d4bde73b67bc52fd26c4f92e0db0932252d7044
lucianoandreslugani-dev <lucianoandreslugani@gmail.com>
2026-06-04 14:15:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,20 @@ def actualizar_producto(id: int, producto: Producto):
     return {"mensaje": "Producto actualizado correctamente", "producto": producto}
 
 
+@app.put("/productos/{id}")
+def actualizar_producto(id: int, producto: Producto):
+    if id < 0 or id >= len(productos):
+        return {"error": "Producto no encontrado"}
+    productos[id] = producto
+    return {"mensaje": "Producto actualizado correctamente", "producto": producto}
+
+
+
+@app.put("/productos/{id}")
+def actualizar_producto(id: int, producto: Producto):
+    if id < 0 or id >= len(productos):
+        return {"error": "Producto no encontrado"}
+    productos[id] = producto
+    return {"mensaje": "Producto actualizado correctamente", "producto": producto}
+
+
